b04e11e87ceb38db82cfaabbc486c24630ae30f6
El Hadji Babacar Diop <bdiop@edilex.com>
2019-02-02 08:56:48 -0500

    add streaming media plugin

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -5,12 +5,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
+  ],
+  providers: [
+    StreamingMedia
   ],
   declarations: [Tab2Page]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page {
+  constructor(private streamingMedia: StreamingMedia) { }
+
+  public options: StreamingVideoOptions = {
+    successCallback: () => { console.log('Video played') },
+    errorCallback: (e) => { console.log('Error streaming') },
+    orientation: 'landscape',
+    shouldAutoClose: true,
+    controls: false
+  };
+
+  play(){
+    this.streamingMedia.playVideo('https://path/to/video/stream', this.options);
+  }
+  
+}
